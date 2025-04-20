18e0180540de20f90342eebebb63f05de7ccc3ac
Kaushar45 <45kaushar@gmail.com>
2025-04-20 20:55:08 +0530

    created login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const loginPage = () => {
-  return <div>Login Page</div>;
+  return (
+    <>
+      <form
+        className="grid items-center justify-center gap-5 "
+        action="onSubmit"
+      >
+        <h1 className="text-2xl font-bold text-blue-950">User Login</h1>
+
+        <input
+          className="border rounded-2xl px-2 text-xl"
+          required
+          placeholder="Username"
+          name="Username"
+        />
+        <input
+          className="border rounded-2xl px-2 text-xl"
+          required
+          placeholder="Password"
+          name="password"
+          type="password"
+        />
+        <button className="border rounded-2xl px-2 text-xl" type="submit">
+          Login
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default loginPage;
