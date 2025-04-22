1c44d1b2e685db4eb54caca3182c611fad1fd7f1
Kaushar45 <45kaushar@gmail.com>
2025-04-22 16:45:06 +0530

    styling login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -4,27 +4,32 @@ const loginPage = () => {
   return (
     <>
       <form
-        className="grid items-center justify-center gap-5 "
+        className="grid items-center justify-center gap-5 bg-[url('https://images.unsplash.com/photo-1699462515761-90db271d77c8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center h-screen "
         action="onSubmit"
       >
-        <h1 className="text-2xl font-bold text-blue-950">User Login</h1>
+        <div className="grid items-center justify-center gap-5 px-6 py-6 rounded-xl  border-1 border-gray-500 bg-[#9595951d] shadow-2xl">
+          <h1 className="text-2xl font-bold text-blue-950">User Login</h1>
 
-        <input
-          className="border rounded-2xl px-2 text-xl"
-          required
-          placeholder="Username"
-          name="Username"
-        />
-        <input
-          className="border rounded-2xl px-2 text-xl"
-          required
-          placeholder="Password"
-          name="password"
-          type="password"
-        />
-        <button className="border rounded-2xl px-2 text-xl" type="submit">
-          Login
-        </button>
+          <input
+            className="border-gray-800 rounded-2xl px-4 text-lg bg-[#03335f1d] "
+            required
+            placeholder="Username"
+            name="Username"
+          />
+          <input
+            className="rounded-2xl px-4 text-lg bg-[#03335f1d] "
+            required
+            placeholder="Password"
+            name="password"
+            type="password"
+          />
+          <button
+            className="rounded-2xl bg-[#03335f1d]  outline-neutral-50 px-4 text-lg"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
       </form>
     </>
   );
