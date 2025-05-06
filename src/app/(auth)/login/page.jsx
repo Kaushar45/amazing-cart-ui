bf795207b030e4d448c6b0a6caa7bd225f6fdbd8
Kaushar45 <45kaushar@gmail.com>
2025-05-06 19:34:01 +0530

    biolar palate of login & sign up

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,19 +1,33 @@
 "use client";
+import { Eye, EyeOff } from "lucide-react";
 import { useGlobalContext } from "../../../context/GlobalContext";
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
+import Link from "next/link";
 
 const loginPage = () => {
+  const router = useState();
   const { setIsLogin } = useGlobalContext();
-  const login = () => {
+  const [isPassword, setIsPassword] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const enableDisableBtn = () => {
+    if (!email.length || !password.length) {
+      return true;
+    }
+    return false;
+  };
+
+  const login = (e) => {
+    e.preventDefault();
     console.log("You are Log in Now");
-    setIsLogin(true);
   };
 
   return (
     <>
       <form
         className="grid items-center justify-center gap-5 bg-[url('https://images.unsplash.com/photo-1699462515761-90db271d77c8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center h-screen "
-        action="onSubmit"
+        onSubmit={login}
       >
         <div className="grid items-center justify-center gap-5 px-6 py-6 rounded-xl  border-1 border-gray-500 bg-[#9595951d] shadow-2xl">
           <h1 className="text-2xl font-bold text-blue-950">User Login</h1>
@@ -23,27 +37,61 @@ const loginPage = () => {
             required
             placeholder="Username"
             name="Username"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <input
-            className="rounded-2xl px-4 text-lg bg-[#03335f1d] "
-            required
-            placeholder="Password"
-            name="password"
-            type="password"
-          />
+          <div className="relative">
+            <input
+              className="rounded-2xl px-4 text-lg bg-[#03335f1d] "
+              required
+              placeholder="Password"
+              name="password"
+              type={isPassword ? "password" : "text"}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {isPassword ? (
+              <Eye
+                className="absolute right-2 top-[50%] -translate-y-[50%] hover:cursor-pointer"
+                onClick={() => setIsPassword(!isPassword)}
+              />
+            ) : (
+              <EyeOff
+                className="absolute right-2 top-[50%] -translate-y-[50%] hover:cursor-pointer"
+                onClick={() => setIsPassword(!isPassword)}
+              />
+            )}
+          </div>
+          <Link
+            href="/forgot_password"
+            className="text-sm text-right text-blue-500"
+          >
+            Forgot Password?
+          </Link>
           <button
-            className="rounded-2xl bg-[#03335f1d]  outline-neutral-50 px-4 text-lg"
+            type="submit"
+            disabled={enableDisableBtn() ? true : false}
+            className={`${
+              enableDisableBtn()
+                ? "bg-gray-300 text-gray-100 "
+                : "bg-[#03335f1d] duration-200 hover:bg-[#13212f1d]"
+            }rounded-2xl cursor-pointer outline-neutral-50 px-4 text-lg`}
             onClick={login}
           >
             Login
           </button>
+          <p className="text-sm">
+            Don't have an account?
+            <Link href="/signup" className="text-blue-500">
+              Create Account
+            </Link>
+          </p>
         </div>
       </form>
       <button
         className="rounded-2xl bg-[#03335f1d]  outline-neutral-50 px-4 text-lg"
         onClick={login}
       >
-        Login
+        Log In
       </button>
     </>
   );
