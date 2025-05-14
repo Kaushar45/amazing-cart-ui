cfe1452ea051699b604d17fe60c281ce1c3631a6
Kaushar45 <45kaushar@gmail.com>
2025-05-14 20:41:48 +0530

    Login Final

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -9,7 +9,14 @@ import { apiClient } from "../../../utils/apiClient";
 const loginPage = () => {
   const router = useState();
   const { setIsLogin } = useGlobalContext();
+  const [isLoading, setIsLoading] = useState(false);
   const [isPassword, setIsPassword] = useState(true);
+  const [validationError, setValidationError] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const enableDisableBtn = () => {
@@ -19,8 +26,38 @@ const loginPage = () => {
     return false;
   };
 
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  };
+
+  const validatePassword = (password) => {
+    const re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return re.test(password);
+  };
   const handleLogin = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
+    setError("");
+    setValidationError({ email: "", password: "" });
+    if (!validateEmail(email)) {
+      setValidationError((prev) => ({
+        ...prev,
+        email: "Please enter a valid email address",
+      }));
+      setIsLoading(false);
+      return;
+    }
+
+    if (!validatePassword(password)) {
+      setValidationError((prev) => ({
+        ...prev,
+        password:
+          "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number and one special character",
+      }));
+      setIsLoading(false);
+      return;
+    }
     try {
       const data = await apiClient.login({
         email: email,
@@ -28,17 +65,45 @@ const loginPage = () => {
       });
       console.log(data);
       if (data.error) {
-        alert(data.message);
+        setError(data.message);
         return;
       }
       setCookie("access_token", data.access_token);
       setCookie("refresh_token", data.refresh_token);
+      const {
+        access_token,
+        refresh_token,
+        refresh_token_expires_at,
+        access_token_expires_at,
+      } = data;
+
+      const currentMilies = Date.now();
+      const accesTokenExpiresAt = Date.parse(access_token_expires_at);
+      const refreshTokenExpiresAt = Date.parse(refresh_token_expires_at);
+
+      setCookie(
+        "access_token",
+        access_token,
+        parseInt(`${(accesTokenExpiresAt - currentMilies) / 1000}`)
+      );
+      setCookie(
+        "refresh_token",
+        refresh_token,
+        parseInt(`${(refreshTokenExpiresAt - currentMilies) / 1000}`)
+      );
       setIsLogin(true);
       setEmail("");
       setPassword("");
       router.push("/");
+      setValidationError({ email: "", password: "" });
+      setError("");
+      setIsLoading(false);
+      router.push("/", { replace: true });
     } catch (error) {
       console.log();
+      setIsLoading(false);
+      setError("Something went wrong. Please try again later.");
+      setValidationError({ email: "", password: "" });
     }
   };
 
@@ -50,9 +115,11 @@ const loginPage = () => {
       >
         <div className="grid items-center justify-center gap-5 px-6 py-6 rounded-xl  border-1 border-gray-500 bg-[#9595951d] shadow-2xl">
           <h1 className="text-2xl font-bold text-blue-950">User Login</h1>
-
+          {error && (
+            <p className="text-sm text-red-500 text-center mt-1">{error}</p>
+          )}
           <input
-            className="border-gray-800 rounded-2xl px-4 text-lg bg-[#03335f1d] "
+            className="border-gray-800 rounded-2xl px-4 text-lg mt-5 bg-[#03335f1d] "
             required
             placeholder="Username"
             name="Username"
@@ -60,6 +127,9 @@ const loginPage = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {validationError.email && (
+            <p className="text-sm text-red-500">{validationError.email}</p>
+          )}
           <div className="relative">
             <input
               className="rounded-2xl px-4 text-lg bg-[#03335f1d] "
@@ -82,6 +152,9 @@ const loginPage = () => {
               />
             )}
           </div>
+          {validationError.password && (
+            <p className="text-sm text-red-500">{validationError.password}</p>
+          )}
           <Link
             href="/forgot_password"
             className="text-sm text-right text-blue-500"
@@ -95,9 +168,17 @@ const loginPage = () => {
               enableDisableBtn()
                 ? "bg-gray-300 text-gray-100 "
                 : "bg-[#03335f1d] duration-200 hover:bg-[#13212f1d]"
-            }rounded-2xl cursor-pointer outline-neutral-50 px-4 text-lg`}
+            }rounded-2xl cursor-pointer outline-neutral-50 px-4 text-lg flex items-center justify-center`}
           >
             Login
+            {isLoading ? (
+              <svg
+                className="animate-spin h-8 w-8 border-t-transparent border-2 rounded-full"
+                viewBox="0 0 24 24"
+              ></svg>
+            ) : (
+              "Login"
+            )}
           </button>
           <p className="text-sm">
             Don't have an account?
