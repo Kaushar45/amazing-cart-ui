ba45dcd0a529cc3f93450275f066ee7172f03591
Kaushar45 <45kaushar@gmail.com>
2025-05-22 20:58:59 +0530

    added delete cookie

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -3,33 +3,29 @@ export function setCookie(name, value, days = 3600) {
   const date = new Date();
   date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
   const expires = "expires=" + date.toUTCString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  if (typeof window !== "undefined") {
+    // browser code
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+  }
 }
 
 // Function to get a cookie by name
 export function getCookie(name) {
   const nameEQ = name + "=";
-  const cookies = document.cookie.split(";");
-  for (let i = 0; i < cookies.length; i++) {
-    let cookie = cookies[i].trim();
-    if (cookie.indexOf(nameEQ) === 0) {
-      return cookie.substring(nameEQ.length, cookie.length);
+  if (typeof window !== "undefined") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i].trim();
+      if (cookie.indexOf(nameEQ) === 0) {
+        return cookie.substring(nameEQ.length, cookie.length);
+      }
     }
   }
   return null;
 }
 
-// Function to check login status
-function checkLogin() {
-  const user = getCookie("username");
-  if (user) {
-    alert("Welcome back, " + user + "!");
-  } else {
-    alert("Please log in.");
-  }
+function deleteCookie(name) {
+  document.cookie = name + "=; path=/; Max-Age=-99999999;";
 }
 
-export function login(username) {
-  setCookie("username", username, 7);
-  alert("Login successful! Welcome, " + username);
-}
+export { setCookie, getCookie, deleteCookie };
